9ce84a76de9758aa471d23c5c7ed40e935b603d9
Mannat Bansal <85332790+MannatBansal@users.noreply.github.com>
2024-03-21 17:11:12 +0530

    Update Spinner.js

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,14 +2,11 @@ import React from 'react'
 import loading from '../loading.gif'
 
 const Spinner = () => {
-// export class Spinner extends Component {
-//   render() {
     return (
       <div className='text-center'>
         <img src={loading} alt="loading" />
       </div>
     )
-//  }}
 }
 
 export default Spinner
